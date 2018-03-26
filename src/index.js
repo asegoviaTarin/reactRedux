db26fd603aa2852e5256662861a80616938f01a3
Unknown <axseta@gmail.com>
2018-03-26 20:23:25 +0200

    api key

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyAxWaUt8lt7d8O1Pf-KIS1Rn-Z_BJApmo4';
 const App = function app() {
   return <div>Hi!</div>;
 };
